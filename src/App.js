dc9396598aa931214a54fcaef68c09586533d67b
Leancher <Leancher@yandex.ru>
2018-12-24 07:00:28 +0300

    ..

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react'; // подключение библиотеки React
-import ReactDOM from 'react-dom';
+import React from 'react' // подключение библиотеки React
+import ReactDOM from 'react-dom'
 //import { Add } from './components/Add' // ./ = текущая директория,
 //import { News } from './components/News'
-import './App.css'; // подключение файла стилей
+import './App.css' // подключение файла стилей
 import { PageMain } from './components/PageMain'
 import { PageContent } from './components/PageContent'
 
@@ -12,14 +12,13 @@ class App extends React.Component {
 
     //news: null,
     //isLoading: false
-
-  };
+  }
   handleAddNews = data => {
     //const nextNews = [data, ...this.state.news];
     //this.setState({ news: nextNews });
-  };
+  }
   componentDidMount() {
-/*     this.setState({isLoading: true})
+    /*     this.setState({isLoading: true})
     fetch('http://localhost:3000/data/newsData.json')
     .then(response => {
       return response.json()
@@ -29,37 +28,49 @@ class App extends React.Component {
         this.setState({ isLoading: false, news: data })
         }, 1000)
     }) */
-    
   }
   render() {
     //const { news, isLoading } = this.state
-    const category = [ 'MyProjects', 'RepairCar', 'MyPhoto', 'ThingUSSR', 'Thing90', 'HistCher', 'MyNotes' ]
+    const category = [
+      'MyProjects',
+      'RepairCar',
+      'MyPhoto',
+      'ThingUSSR',
+      'Thing90',
+      'HistCher',
+      'MyNotes',
+    ]
     return (
       <React.Fragment>
-        <div className='header'>
+        <div className="header">
           <div className="HeaderTitle">
-            <a href ="index.html">
-                <img src="./Pictures/Logo/Main.png" alt='alt'/>
-                {/* <asp:Image ID="LogoPic" runat="server" /> */}
-                LEANCHER 
+            <a href="index.html">
+              <img src="./Pictures/Logo/Main.png" alt="alt" />
+              {/* <asp:Image ID="LogoPic" runat="server" /> */}
+              LEANCHER
             </a>
           </div>
-          <div className='HeaderLink'>
-            <a href='#linkStat' className='HeaderMenu'>Статистика</a><br />
-            <a href='#linkAbout' className='HeaderMenu'>О сайте</a>
+          <div className="HeaderLink">
+            <a href="#linkStat" className="HeaderMenu">
+              Статистика
+            </a>
+            <br />
+            <a href="#linkAbout" className="HeaderMenu">
+              О сайте
+            </a>
           </div>
         </div>
-        <div className = 'Body' >
-          <PageContent category = {category} />
+        <div className="Body">
+          <PageContent category={category} />
         </div>
 
-{/*         <Add onAddNews={this.handleAddNews} />
+        {/*         <Add onAddNews={this.handleAddNews} />
         <h3>Новости</h3>
         {isLoading && <p>Загружаю...</p>}
         {Array.isArray(news) ? <News data = {news} /> : <p>Нет новостей</p>} */}
       </React.Fragment>
-    );
+    )
   }
 }
 
-export default App;
+export default App
