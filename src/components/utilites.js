1dcce00617a8d78bd26845baa576b12f5bf540b2
Leancher <Leancher@yandex.ru>
2019-03-07 18:57:06 +0300

    ..

--- src/components/utilites.js
+++ src/components/utilites.js
@@ -1,7 +1,7 @@
 export const config = {
   siteName: 'Leancher Web site',
-  serverUrl: 'http://localhost:53492/Server.aspx',
-  //serverUrl: 'http://192.168.0.100:8090/Server.aspx',
+  //serverUrl: 'http://localhost:53492/Server.aspx',
+  serverUrl: 'http://192.168.0.100:8090/Server.aspx',
   name: 0,
   catNum: 1,
   subCatNum: 2,
